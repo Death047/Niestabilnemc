3052b2ed1a4053da8a9421c0a1bb079bd0737c61
Death047 <maksgorzen@gmail.com>
2026-09-17 16:14:26 +0000

    dodaj prosty serwer

---
+++ index.js
@@ -0,0 +1,13 @@
+cat << 'EOF' > index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Niestabilnemc dziala!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+EOF
